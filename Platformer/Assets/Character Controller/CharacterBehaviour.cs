c45de92ce5e11619813a112711e8a0277f60cc47
Zackery Elmer <Zackeryelmer@comcast.net>
2024-10-04 18:33:56 -0600

    Finished Platformer Part 1

---
+++ Platformer/Assets/Character Controller/CharacterBehaviour.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBehaviour : MonoBehaviour
+{
+    private CharacterController controller;
+    public CharacterPattern characterPattern;
+    private WaitForFixedUpdate waitObj;
+    public UnityEvent awakeEvent, triggerEnterEvent, triggerExitEvent;
+    private bool canRun = true;
+    public bool CanRun{
+        get{
+            return canRun;
+        }
+        set{
+            canRun = value;
+        }
+    }
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+        waitObj = new WaitForFixedUpdate();
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void SwapPattern(CharacterPattern pattern)
+    {
+        characterPattern = pattern;
+    }
+
+    /*public IEnumerator MoveCharacter(){
+        while (canRun == true){
+            characterPattern.Move(controller);
+            yield return waitObj;
+        }
+    }*/
+
+    public void LateUpdate()
+    {
+        if(canRun == true){
+            characterPattern.Move(controller);
+        }
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+}
---
+++ Platformer/Assets/Scripts/DeathBehavior.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeathBehavior : MonoBehaviour
+{
+
+    public UnityEvent onDeathEvent, delayedDeathEvent;
+    private WaitForSeconds wfsObj;
+
+    private void Awake(){
+        wfsObj = new WaitForSeconds(3);
+    }
+    private IEnumerator OnTriggerEnter(Collider other){
+        if (other.tag == "Death"){
+            Debug.Log("Deaded");
+            onDeathEvent.Invoke();
+            yield return wfsObj;
+            delayedDeathEvent.Invoke();
+        }
+    }
+}
